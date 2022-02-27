12e0e28dd35bf892ca7ca5dfdb97113d1b3dc097
wyl <1064902365@qq.com>
2022-02-27 20:48:17 +0800

    init

---
+++ app/src/main/java/com/xl/mycompose/MainActivity.kt
@@ -0,0 +1,134 @@
+package com.xl.mycompose
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.xl.mycompose.ui.theme.MyComposeTheme
+
+class MainActivity : ComponentActivity() {
+
+    @ExperimentalMaterialApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+
+@ExperimentalMaterialApi
+@Preview(showBackground = true)
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(topBar = { TitleBar() }, bottomBar = { BottomNavigationBar(navController) }) {
+        Log.e("TAG", "MainScreen: ")
+        Navigation(navController = navController)
+    }
+}
+
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(navController, startDestination = NavigationItem.Home.route) {
+        composable(NavigationItem.Home.route) {
+
+        }
+        composable(NavigationItem.Movies.route) {
+            MoviesScreen()
+        }
+        composable(NavigationItem.Books.route) {
+            BooksScreen()
+        }
+        composable(NavigationItem.Profile.route) {
+            ProfileScreen()
+        }
+    }
+}
+
+
+@Composable
+fun TitleBar() {
+    TopAppBar(
+        title = { Text(text = "Compose", fontSize = 18.sp) },
+        backgroundColor = colorResource(id = R.color.purple_200),
+        contentColor = Color.White
+    )
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val items = listOf(
+        NavigationItem.Home,
+        NavigationItem.Movies,
+        NavigationItem.Books,
+        NavigationItem.Profile
+    )
+    BottomNavigation(
+        backgroundColor = Color.Blue,
+        contentColor = Color.White
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = {
+                    if (item.messageCount != 0) {
+                        BadgeBox(backgroundColor = Color.White,
+                            contentColor = Color.Red,
+                            badgeContent = { Text(text = "8") }
+                        ) {
+                            Icon(
+                                ImageVector.Companion.vectorResource(id = item.icon),
+                                contentDescription = item.title
+                            )
+                        }
+                    } else {
+                        Icon(painterResource(id = item.icon), contentDescription = item.title)
+                    }
+                },
+                label = { Text(text = item.title) },
+                selectedContentColor = Color.Red,
+                unselectedContentColor = Color.White.copy(0.4f),
+                alwaysShowLabel = true,
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    }
+                },
+            )
+        }
+
+    }
+}
